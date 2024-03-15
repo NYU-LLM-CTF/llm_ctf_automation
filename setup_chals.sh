12754faadf19dc3433b6391cad1e87c9322f311d
NickNameInvalid <zssmh98@outlook.com>
2024-03-15 19:27:51 +0000

    1

--- setup_chals.sh
+++ setup_chals.sh
@@ -1,30 +1,41 @@
-while getopts ":y:e:t:c:" opt
-do
+#!/bin/bash
+
+valid_opts=0
+
+while getopts ":y:e:t:c:" opt; do
     case $opt in
         y)
         year="$OPTARG"
-        # echo $year
+        valid_opts=$((valid_opts + 1))
         ;;
         e)
         event="$OPTARG"
-        # echo $event
+        valid_opts=$((valid_opts + 1))
         ;;
         t)
         category="$OPTARG"
-        # echo $category
+        valid_opts=$((valid_opts + 1))
         ;;
         c)
         chal="$OPTARG"
-        # echo $chal
+        valid_opts=$((valid_opts + 1))
         ;;
         ?)
-        echo "usage: -y {year} -e {event} -t {category} -c {challenge}"
-        exit 1;;
+        echo "Invalid option: -$OPTARG" >&2
+        echo "Usage: -y {year} -e {event} -t {category} -c {challenge}" >&2
+        exit 1
+        ;;
     esac
 done
 
+if [ $valid_opts -ne 4 ]; then
+    echo "All four options must be provided." >&2
+    echo "Usage: -y {year} -e {event} -t {category} -c {challenge}" >&2
+    exit 1
+fi
+
 chal_path="LLM_CTF_Database/$year/$event/$category/$chal"
-cd $chal_path
-echo "Building $d"
+cd $chal_path || exit
+echo "Building $chal"
 image_name=$(jq -r .name < challenge.json)
 docker build -t "$image_name" "."
