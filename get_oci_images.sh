b7d038f2bf9d818c2a31a5fc91782781528039ce
NickNameInvalid <zssmh98@outlook.com>
2024-04-15 20:47:23 +0000

    1

---
+++ get_oci_images.sh
@@ -0,0 +1,64 @@
+data_dir=/data/ctf_images/validated_oci
+event=2023f
+cp ${data_dir}/${event}/2023f-cry-describeme.tar ./LLM_CTF_Database/2023/CSAW-Finals/crypto/DescribeMe
+cp ${data_dir}/${event}/2023f-cry-nervcenter.tar ./LLM_CTF_Database/2023/CSAW-Finals/crypto/nervcenter
+cp ${data_dir}/${event}/2023f-cry-textbook_rsa.tar ./LLM_CTF_Database/2023/CSAW-Finals/crypto/Textbook-RSA
+cp ${data_dir}/${event}/spill-the-tea.tar ./LLM_CTF_Database/2023/CSAW-Finals/crypto/Spill-The-Tea
+
+cp ${data_dir}/${event}/2023f-for-ancient_forensics.tar ./LLM_CTF_Database/2023/CSAW-Finals/forensics/ancient-forensics
+cp ${data_dir}/${event}/2023f-for-hieroglyphs.tar ./LLM_CTF_Database/2023/CSAW-Finals/forensics/hieroglyphs
+cp ${data_dir}/${event}/2023f-for-keylogger1.tar ./LLM_CTF_Database/2023/CSAW-Finals/forensics/keylogger1
+cp ${data_dir}/${event}/2023f-for-spam.tar ./LLM_CTF_Database/2023/CSAW-Finals/forensics/spam
+
+cp ${data_dir}/${event}/2023f-msc-pyjail.tar ./LLM_CTF_Database/2023/CSAW-Finals/misc/pyjail
+cp ${data_dir}/${event}/2023f-msc-sigmaslogistics.tar ./LLM_CTF_Database/2023/CSAW-Finals/misc/sigmaslogistics
+cp ${data_dir}/${event}/2023f-msc-urkel.tar ./LLM_CTF_Database/2023/CSAW-Finals/misc/urkel
+cp ${data_dir}/${event}/2023f-msc-vectors_machine.tar ./LLM_CTF_Database/2023/CSAW-Finals/misc/vectors_machine
+
+cp ${data_dir}/${event}/2023f-pwn-brainflop.tar ./LLM_CTF_Database/2023/CSAW-Finals/pwn/brainflop
+cp ${data_dir}/${event}/2023f-pwn-rce.tar ./LLM_CTF_Database/2023/CSAW-Finals/pwn/rce
+cp ${data_dir}/${event}/2023f-pwn-virtualization_pwn.tar ./LLM_CTF_Database/2023/CSAW-Finals/pwn/Virtualization-pwn
+
+cp ${data_dir}/${event}/2023f-rev-cell.tar ./LLM_CTF_Database/2023/CSAW-Finals/rev/cell
+
+cp ${data_dir}/${event}/2023f-web-dinoauth.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/dinoauth
+cp ${data_dir}/${event}/2023f-web-ntf-db.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/nft-world
+cp ${data_dir}/${event}/2023f-web-ntf-server.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/nft-world
+cp ${data_dir}/${event}/2023f-web-shreeramquest-nginx.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/SheeRamQuest
+cp ${data_dir}/${event}/2023f-web-shreeramquest-nodeserver.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/SheeRamQuest
+cp ${data_dir}/${event}/2023f-web-triathlon_or_sprint-clientcontainer.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/triathlon_or_sprint
+cp ${data_dir}/${event}/2023f-web-triathlon_or_sprint-servercontainer.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/triathlon_or_sprint
+cp ${data_dir}/${event}/2023f-web-uploaders_revenge-app.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/uploaders_revenge
+cp ${data_dir}/${event}/2023f-web-uploaders_revenge-bot.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/uploaders_revenge
+cp ${data_dir}/${event}/2023f-web-webhooktester.tar ./LLM_CTF_Database/2023/CSAW-Finals/web/webhooktester
+
+event=2023q
+cp ${data_dir}/${event}/2023q-cry-blocky_noncense.tar ./LLM_CTF_Database/2023/CSAW-Quals/crypto/"blocky noncense"
+cp ${data_dir}/${event}/2023q-cry-lottery.tar ./LLM_CTF_Database/2023/CSAW-Quals/crypto/lottery
+cp ${data_dir}/${event}/2023q-cry-mental_poker.tar ./LLM_CTF_Database/2023/CSAW-Quals/crypto/mental-poker
+
+cp ${data_dir}/${event}/2023q-msc-android_dropper.tar ./LLM_CTF_Database/2023/CSAW-Quals/misc/android-dropper
+cp ${data_dir}/${event}/2023q-msc-linear_aggressor.tar ./LLM_CTF_Database/2023/CSAW-Quals/misc/linear_aggressor
+cp ${data_dir}/${event}/2023q-msc-tradinggame.tar ./LLM_CTF_Database/2023/CSAW-Quals/misc/TradingGame
+
+cp ${data_dir}/${event}/2023q-pwn-double_zer0_dilemma.tar ./LLM_CTF_Database/2023/CSAW-Quals/pwn/double_zer0_dilemma
+cp ${data_dir}/${event}/2023q-pwn-my_first_pwnie.tar ./LLM_CTF_Database/2023/CSAW-Quals/pwn/my_first_pwnie
+cp ${data_dir}/${event}/2023q-pwn-puffin.tar ./LLM_CTF_Database/2023/CSAW-Quals/pwn/puffin
+cp ${data_dir}/${event}/2023q-pwn-super_secure_heap.tar ./LLM_CTF_Database/2023/CSAW-Quals/pwn/super_secure_heap
+cp ${data_dir}/${event}/2023q-pwn-target_practice.tar ./LLM_CTF_Database/2023/CSAW-Quals/pwn/"target practice"
+cp ${data_dir}/${event}/2023q-pwn-unlimited_subway.tar ./LLM_CTF_Database/2023/CSAW-Quals/pwn/unlimited_subway
+
+cp ${data_dir}/${event}/2023q-web-cookie_injection.tar ./LLM_CTF_Database/2023/CSAW-Quals/web/cookie-injection
+cp ${data_dir}/${event}/2023q-web-philanthropy-phil-front.tar ./LLM_CTF_Database/2023/CSAW-Quals/web/philanthropy
+cp ${data_dir}/${event}/2023q-web-philanthropy-phil-id.tar ./LLM_CTF_Database/2023/CSAW-Quals/web/philanthropy
+cp ${data_dir}/${event}/2023q-web-philanthropy-phil-nginx.tar ./LLM_CTF_Database/2023/CSAW-Quals/web/philanthropy
+cp ${data_dir}/${event}/2023q-web-rainbow_notes-bot.tar ./LLM_CTF_Database/2023/CSAW-Quals/web/rainbow-notes
+cp ${data_dir}/${event}/2023q-web-smug_dino.tar ./LLM_CTF_Database/2023/CSAW-Quals/web/smug-dino
+
+event=2022f
+cp ${data_dir}/${event}/2022f-cry-open_ellipti_ph.tar ./LLM_CTF_Database/2022/CSAW-Finals/crypto/"open-ELLIPTI-PH!"
+cp ${data_dir}/${event}/2022f-cry-polly_crack_this.tar ./LLM_CTF_Database/2022/CSAW-Finals/crypto/polly-crack-this
+cp ${data_dir}/${event}/2022f-cry-the_lengths_we_extend_ourselves.tar ./LLM_CTF_Database/2022/CSAW-Finals/crypto/"The Lengths we Extend Ourselves"
+cp ${data_dir}/${event}/2022f-for-corporate_ntk_problems.tar ./LLM_CTF_Database/2022/CSAW-Finals/forensics/"Corporate NTK Problems"
+cp ${data_dir}/${event}/2022f-for-oh_shucks_i_need_tosleep_osint.tar ./LLM_CTF_Database/2022/CSAW-Finals/forensics/
+
