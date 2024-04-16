4537e3b5f35223643f6f63afe8dca8ed8b0adf14
sj2790 <sj2790@nyu.edu>
2024-04-16 01:39:08 +0000

    Adding script for organizing oci images and getting source code

---
+++ get-source-code.sh
@@ -0,0 +1,55 @@
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Finals/crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Finals/forensics /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Finals/misc /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Finals/pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Finals/rev /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Finals/web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Finals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Quals/crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Quals/forensics /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Quals/misc /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Quals/pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Quals/rev /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2021-Quals/web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2021/CSAW-Quals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Finals/crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Finals/misc /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Finals/pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Finals/rev /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Finals/web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Finals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Quals/crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Quals/pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Quals/rev /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2020-Quals/web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2020/CSAW-Quals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Finals/crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Finals/misc /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Finals/pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Finals/rev /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Finals/web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Finals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Quals/crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Quals/misc /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Quals/pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Quals/rev /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Quals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2019-Quals/web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2019/CSAW-Quals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2018-Finals/crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2018/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2018-Finals/misc /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2018/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2018-Finals/pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2018/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2018-Finals/rev /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2018/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2018-Finals/web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2018/CSAW-Finals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2014-Finals/Crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2014/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2014-Finals/Forensics /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2014/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2014-Finals/Misc /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2014/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2014-Finals/Pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2014/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2014-Finals/Reversing /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2014/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2014-Finals/Web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2014/CSAW-Finals
+
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2013-Finals/Crypto /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2013/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2013-Finals/Forensics /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2013/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2013-Finals/Pwn /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2013/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2013-Finals/Reversing /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2013/CSAW-Finals
+cp -r /home/sofija/CSAW-CTF/CSAW-CTF-2013-Finals/Web /home/sofija/experiments-llm/llm_ctf_automation/LLM_CTF_Database/2013/CSAW-Finals
---
+++ get_oci_sofija.sh
@@ -0,0 +1,83 @@
+data_dir=/data/ctf_images/validated_oci
+event=2020f
+cp ${data_dir}/${event}/2020f-cry-eccentric.tar ./LLM_CTF_Database/2020/CSAW-Finals/crypto/eccentric
+cp ${data_dir}/${event}/2020f-cry-jackpot.tar ./LLM_CTF_Database/2020/CSAW-Finals/crypto/jackpot
+cp ${data_dir}/${event}/2020f-cry-the_matrix.tar ./LLM_CTF_Database/2020/CSAW-Finals/crypto/the_matrix
+
+cp ${data_dir}/${event}/2020f-msc-wabfs-chunkserver1.tar ./LLM_CTF_Database/2020/CSAW-Finals/misc/wabfs
+cp ${data_dir}/${event}/2020f-msc-wabfs-chunkserver2.tar ./LLM_CTF_Database/2020/CSAW-Finals/misc/wabfs
+cp ${data_dir}/${event}/2020f-msc-wabfs-chunkserver3.tar ./LLM_CTF_Database/2020/CSAW-Finals/misc/wabfs
+cp ${data_dir}/${event}/2020f-msc-wabfs-chunkserver4.tar ./LLM_CTF_Database/2020/CSAW-Finals/misc/wabfs
+cp ${data_dir}/${event}/2020f-msc-wabfs-chunkserver5.tar ./LLM_CTF_Database/2020/CSAW-Finals/misc/wabfs
+cp ${data_dir}/${event}/2020f-msc-wabfs-main.tar ./LLM_CTF_Database/2020/CSAW-Finals/misc/wabfs
+
+cp ${data_dir}/${event}/2020f-pwn-applicative.tar ./LLM_CTF_Database/2020/CSAW-Finals/pwn/applicative
+cp ${data_dir}/${event}/2020f-pwn-darkunion1.tar ./LLM_CTF_Database/2020/CSAW-Finals/pwn/darkunion1
+
+cp ${data_dir}/${event}/2020f-web-comment_anywhere.tar ./LLM_CTF_Database/2020/CSAW-Finals/web/comment_anywhere
+cp ${data_dir}/${event}/2020f-web-picgram.tar ./LLM_CTF_Database/2020/CSAW-Finals/web/picgram
+
+event=2020q
+cp ${data_dir}/${event}/2020q-cry-adversarial.tar ./LLM_CTF_Database/2020/CSAW-Quals/crypto/adversarial
+cp ${data_dir}/${event}/2020q-cry-authy.tar ./LLM_CTF_Database/2020/CSAW-Quals/crypto/authy
+cp ${data_dir}/${event}/2020q-cry-difib.tar ./LLM_CTF_Database/2020/CSAW-Quals/crypto/difib
+cp ${data_dir}/${event}/2020q-cry-modus_operandi.tar ./LLM_CTF_Database/2020/CSAW-Quals/crypto/modus_operandi
+cp ${data_dir}/${event}/2020q-cry-smallsurp.tar ./LLM_CTF_Database/2020/CSAW-Quals/crypto/smallsurp
+
+cp ${data_dir}/${event}/2020q-pwn-feather.tar ./LLM_CTF_Database/2020/CSAW-Quals/pwn/feather
+cp ${data_dir}/${event}/2020q-pwn-grid.tar ./LLM_CTF_Database/2020/CSAW-Quals/pwn/grid
+cp ${data_dir}/${event}/2020q-pwn-roppity.tar ./LLM_CTF_Database/2020/CSAW-Quals/pwn/roppity
+cp ${data_dir}/${event}/2020q-pwn-slithery.tar ./LLM_CTF_Database/2020/CSAW-Quals/pwn/slithery
+cp ${data_dir}/${event}/2020q-pwn-thebardsfail.tar ./LLM_CTF_Database/2020/CSAW-Quals/pwn/thebardsfail
+
+cp ${data_dir}/${event}/2020q-rev-not_malware.tar ./LLM_CTF_Database/2020/CSAW-Quals/rev/not_malware
+
+cp ${data_dir}/${event}/2020q-web-flask_caching.tar ./LLM_CTF_Database/2020/CSAW-Quals/web/flask_caching
+cp ${data_dir}/${event}/2020q-web-real_time_chat.tar ./LLM_CTF_Database/2020/CSAW-Quals/web/real-time-chat
+cp ${data_dir}/${event}/2020q-web-widthless.tar ./LLM_CTF_Database/2020/CSAW-Quals/web/widthless
+
+event=2019f
+cp ${data_dir}/${event}/2019f-cry-ezpz.tar ./LLM_CTF_Database/2019/CSAW-Finals/crypto/ezpz
+cp ${data_dir}/${event}/2019f-cry-macrypto.tar ./LLM_CTF_Database/2019/CSAW-Finals/crypto/macrypto
+cp ${data_dir}/${event}/2019f-cry-sharing_is_caring.tar ./LLM_CTF_Database/2019/CSAW-Finals/crypto/sharing_is_caring
+cp ${data_dir}/${event}/2019f-cry-tvm_2_final.tar ./LLM_CTF_Database/2019/CSAW-Finals/crypto/tvm_2_final
+
+cp ${data_dir}/${event}/2019f-pwn-arevenge.tar ./LLM_CTF_Database/2019/CSAW-Finals/pwn/arevenge
+cp ${data_dir}/${event}/2019f-pwn-autobots1_1.tar ./LLM_CTF_Database/2019/CSAW-Finals/pwn/autobots1.1
+cp ${data_dir}/${event}/2019f-pwn-defile.tar ./LLM_CTF_Database/2019/CSAW-Finals/pwn/defile
+cp ${data_dir}/${event}/2019f-pwn-quirky_emulation.tar ./LLM_CTF_Database/2019/CSAW-Finals/pwn/quirky-emulation
+cp ${data_dir}/${event}/2019f-pwn-superfly.tar ./LLM_CTF_Database/2019/CSAW-Finals/pwn/superfly
+cp ${data_dir}/${event}/2019f-pwn-superfly2.tar ./LLM_CTF_Database/2019/CSAW-Finals/pwn/superfly2
+cp ${data_dir}/${event}/2019f-pwn-unfork.tar ./LLM_CTF_Database/2019/CSAW-Finals/pwn/unfork
+
+cp ${data_dir}/${event}/2019f-rev-mystery_boi.tar ./LLM_CTF_Database/2019/CSAW-Finals/rev/mystery_boi
+cp ${data_dir}/${event}/2019f-rev-wrangly.tar ./LLM_CTF_Database/2019/CSAW-Finals/rev/wrangly
+
+cp ${data_dir}/${event}/2019f-web-biometric-biometric_server.tar ./LLM_CTF_Database/2019/CSAW-Finals/web/biometric
+cp ${data_dir}/${event}/2019f-web-very_saasy-sql-challenge.tar ./LLM_CTF_Database/2019/CSAW-Finals/web/very_SaaSy
+
+event=2019q
+cp ${data_dir}/${event}2019q/2019q-cry-brillouin.tar ./LLM_CTF_Database/2019/CSAW-Quals/crypto/brillouin 
+cp ${data_dir}/${event}2019q/2019q-cry-byte_me.tar ./LLM_CTF_Database/2019/CSAW-Quals/crypto/byte_me
+cp ${data_dir}/${event}2019q/2019q-cry-count_on_me.tar ./LLM_CTF_Database/2019/CSAW-Quals/crypto/count_on_me
+cp ${data_dir}/${event}2019q/2019q-cry-super_curve.tar ./LLM_CTF_Database/2019/CSAW-Quals/crypto/super_curve
+cp ${data_dir}/${event}2019q/2019q-cry-time_lie.tar ./LLM_CTF_Database/2019/CSAW-Quals/crypto/time.lie
+
+cp ${data_dir}/${event}2019q/2019q-pwn-baby_boi.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/baby_boi
+cp ${data_dir}/${event}2019q/2019q-pwn-got_milk.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/got_milk
+cp ${data_dir}/${event}2019q/2019q-pwn-popping_caps_2.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/popping_caps_2
+cp ${data_dir}/${event}2019q/2019q-pwn-popping_caps.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/popping_caps
+cp ${data_dir}/${event}2019q/2019q-pwn-small_boi.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/small_boi
+cp ${data_dir}/${event}2019q/2019q-pwn-traveller.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/traveller
+cp ${data_dir}/${event}2019q/2019q-pwn-tvm.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/tvm
+cp ${data_dir}/${event}2019q/2019q-pwn-twitch_plays_shellcode.tar ./LLM_CTF_Database/2019/CSAW-Quals/pwn/twitch_plays_shellcode
+
+cp ${data_dir}/${event}2019q/2019q-rev-call_site.tar ./LLM_CTF_Database/2019/CSAW-Quals/rev/call_site
+cp ${data_dir}/${event}2019q/2019q-rev-gibberish_check.tar ./LLM_CTF_Database/2019/CSAW-Quals/rev/gibberish_check
+cp ${data_dir}/${event}2019q/2019q-rev-wizkid.tar ./LLM_CTF_Database/2019/CSAW-Quals/rev/wizkid
+
+cp ${data_dir}/${event}2019q/2019q-web-babycsp.tar ./LLM_CTF_Database/2019/CSAW-Quals/web/babycsp
+cp ${data_dir}/${event}2019q/2019q-web-buyify.tar ./LLM_CTF_Database/2019/CSAW-Quals/web/buyify
+cp ${data_dir}/${event}2019q/2019q-web-saasy-sql-challenge.tar ./LLM_CTF_Database/2019/CSAW-Quals/web/saasy
+cp ${data_dir}/${event}2019q/2019q-web-secure_file_store.tar ./LLM_CTF_Database/2019/CSAW-Quals/web/secure_file_store
+cp ${data_dir}/${event}2019q/2019q-web-unagi.tar ./LLM_CTF_Database/2019/CSAW-Quals/web/unagi
