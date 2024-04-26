47a91f069d19012131862f1f85293848d996154a
Brendan Dolan-Gavitt <brendandg@nyu.edu>
2024-04-26 09:34:36 -0400

    More features for llm_solve.sh
    - Allow arbitrary arguments to be passed down to llm_ctf_solve.py
      by placing them at the end, after a --
    - Replace / by _ in the model name (e.g. meta-llama/Meta-Llama-3-70B-Instruct)
    - Allow specifying number of trials with -T (defaults to 5)
    - Add a -f option to force the eval to run even if a log already
      exists (will overwrite).
    - Minor: don't try to zero-pad the attempt number since trials is usually
      single digit now.

--- llm_solve.sh
+++ llm_solve.sh
@@ -2,7 +2,11 @@
 
 valid_opts=0
 
-while getopts ":y:e:t:c:m:" opt; do
+# Default values
+trials=5 # Number of trials to run
+force=0 # Whether to force re-running the solver even if the log exists
+
+while getopts ":y:e:t:c:m:T:f" opt; do
     case $opt in
         y)
         year="$OPTARG"
@@ -24,9 +28,15 @@ while getopts ":y:e:t:c:m:" opt; do
         model="$OPTARG"
         valid_opts=$((valid_opts + 1))
         ;;
+        T)
+        trials="$OPTARG"
+        ;;
+        f)
+        force=1
+        ;;
         ?)
         echo "Invalid option: -$OPTARG" >&2
-        echo "Usage: -y {year} -e {event} -t {category} -c {challenge} -m model" >&2
+        echo "Usage: -y {year} -e {event} -t {category} -c {challenge} -m model [-T {trials=5}] [-- [llm_solve_args]]" >&2
         exit 1
         ;;
     esac
@@ -34,9 +44,13 @@ done
 
 if [ $valid_opts -ne 5 ]; then
     echo "All four options must be provided." >&2
-    echo "Usage: -y {year} -e {event} -t {category} -c {challenge} -m {model}" >&2
+    echo "Usage: -y {year} -e {event} -t {category} -c {challenge} -m {model} [-T {trials=5}] [-- [llm_solve_args]]" >&2
     exit 1
 fi
+
+# Pop off everything but the args we want to pass to the solver
+shift $(($OPTIND - 1))
+echo "Extra args to llm_ctf_solve.py: $*"
 
 function cleanup_container {
     docker stop ctfenv &> /dev/null
@@ -49,15 +63,20 @@ function cleanup_container {
 }
 chal_path="LLM_CTF_Database/${year}/${event}/${category}/${chal}"
 echo "Start evaluation on ${year}/${event}/${category}/${chal}"
-epoch=5
 # bash setup_chals.sh -y "${year}" -e "${event}" -t "${category}" -c "${chal}"
-for i in {1..5}; do
-    log="logs/${year}/${event}/${category}/${chal}/conversation.${model}.${i}.json"
+safe_model=$(echo "${model}" | tr '/' '_')
+for i in $(seq 1 $trials) ; do
+    log="logs/${year}/${event}/${category}/${chal}/conversation.${safe_model}.${i}.json"
     if [ -f "${log}" ]; then
-        printf '[%02d/10] skipping %s attempting %s for challenge /%s/%s/%s/%s; log exists\n' $i "${model}" "${year}" "${event}" "${category}" "${chal}"
-        continue
+        if [ $force -eq 1 ]; then
+            echo "Removing existing log file as requested: ${log}"
+            rm "${log}"
+        else
+            printf '[%d/%d] skipping %s attempting %s for challenge /%s/%s/%s/%s; log exists\n' $i $trials "${model}" "${year}" "${event}" "${category}" "${chal}"
+            continue
+        fi
     fi
     cleanup_container
-    printf '[%02d/5] %s attempting %s for challenge /%s/%s/%s/%s\n' $i "${model}" "${year}" "${event}" "${category}" "${chal}"
-    python llm_ctf_solve.py -d -M ${model} -m 30 -L "${log}" "${chal_path}/challenge.json"
+    printf '[%d/%d] %s attempting %s for challenge /%s/%s/%s/%s\n' $i $trials "${model}" "${year}" "${event}" "${category}" "${chal}"
+    python llm_ctf_solve.py -d -M ${model} -m 30 -L "${log}" "${chal_path}/challenge.json" "$@"
 done
