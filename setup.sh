d37ab47582505471d06ef73f937cdc9e743085f9
NickNameInvalid <zssmh98@outlook.com>
2024-07-04 07:20:54 -0400

    1

--- setup.sh
+++ setup.sh
@@ -3,21 +3,21 @@
 # Build main docker image
 docker build --build-arg HOST_UID=$(id -u) -t ctfenv .
 
-# Build docker image for each challenge
-for d in LLM_CTF_Database/{rev}/*; do
-    if [ -d "$d" ]; then
-        echo "Building $d"
-        image_name=$(jq -r .container_image < "$d"/challenge.json)
-        is_compose=$(jq -r .compose < "$d"/challenge.json)
-        if [ "$is_compose" = "true" ]; then
-            docker compose -f "$d"/docker-compose.yml build
-        elif [ "$image_name" = "null" ]; then
-            continue
-        else
-            docker build -t "$image_name" "$d"
-        fi
-    fi
-done
+# # Build docker image for each challenge
+# for d in LLM_CTF_Database/{rev}/*; do
+#     if [ -d "$d" ]; then
+#         echo "Building $d"
+#         image_name=$(jq -r .container_image < "$d"/challenge.json)
+#         is_compose=$(jq -r .compose < "$d"/challenge.json)
+#         if [ "$is_compose" = "true" ]; then
+#             docker compose -f "$d"/docker-compose.yml build
+#         elif [ "$image_name" = "null" ]; then
+#             continue
+#         else
+#             docker build -t "$image_name" "$d"
+#         fi
+#     fi
+# done
 
 networkname="ctfnet"
 network_exists=$(docker network ls | grep $networkname)
