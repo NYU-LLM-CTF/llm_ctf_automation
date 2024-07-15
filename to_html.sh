898d9dcc473e0dcf702c4a523469fa50a6605bed
NickNameInvalid <zssmh98@outlook.com>
2024-07-15 10:37:43 -0400

    update dump command

--- to_html.sh
+++ to_html.sh
@@ -1,6 +1,6 @@
 MODEL="$1"
-for f in logs/*/*/*/*.${MODEL}.*.json ; 
-do outdir=./public_html/$(dirname "$f") ; 
+for f in logs/*/*/*/*/*.${MODEL}.*.json ; 
+do outdir=./html/$(dirname "$f") ; 
 mkdir -p "$outdir" ; outname="$(basename "$f")" ; 
 outname="${outname/.json/.html}"; echo "$f" ; 
-python ./llm_ctf/dump_commands.py "$f" | ansi2html -l -s osx | sed 's/color: #AAAAAA;/color: #FFFFFF;/g' > "$outdir"/"$outname" ; done
+python ./llm_ctf/dump_commands.py "$f" | ansi2html -l | sed 's/color: #AAAAAA;/color: #FFFFFF;/g' > "$outdir"/"$outname" ; done
